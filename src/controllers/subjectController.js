c1fd18debc9df6465151fb252646f3ae2be94879
yirex <mraibel12@gmail.com>
2024-09-06 17:43:12 -0400

    subject controller

--- src/controllers/subjectController.js
+++ src/controllers/subjectController.js
@@ -1,28 +1,57 @@
 const Subject = require('../models/subjectModel')
+const Grade = require('../models/gradeModel')
+const Subgrade = require('../models/subgradeModel')
 
 const getSubjectsBySemesterId = async (req, res) => {
-    
+
     const { id } = req.params
 
     try {
-        
+
         const semesters = await Subject.findAll({
-            where:{
+            where: {
                 id_semester: id
             }
         })
-    
-        if(semesters < 0 ) {
+
+        if (semesters < 0) {
             res.status(400).json({ message: "El estudiante no tiene semestres asociados" })
         }
 
         res.status(200).json(semesters)
 
-    } catch(e) {
+    } catch (e) {
         res.status(500).json({ message: "No se pudieron obtener los semestres" })
     }
 }
 
+const getSubjectById = async (req, res) => {
+
+    const { id } = req.params
+
+    try {
+
+        const subject = await Subject.findByPk(id, {
+            include:[
+                {
+                    model:Grade,
+                    include:[{model:Subgrade}]
+                },
+            ]
+        })
+
+        if(!subject){
+            res.status(400).json({ message: 'No existe asignatura con este id' })
+        }
+
+        res.status(200).json(subject)
+
+    } catch (e) {
+        res.status(500).json({ message: 'No se pudo obtener la asignatura' })
+    }
+}
+
 module.exports = {
-    getSubjectsBySemesterId
+    getSubjectsBySemesterId,
+    getSubjectById
 }
--- src/routes/subjectRoutes.js
+++ src/routes/subjectRoutes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express")
-const { getSubjectsBySemesterId } = require('../controllers/subjectController')
+const { getSubjectsBySemesterId, getSubjectById } = require('../controllers/subjectController')
 
 
 const router = Router()
 
 router.get("/:id", getSubjectsBySemesterId)
+
+router.get('/grade/:id', getSubjectById)
 
 module.exports = router
